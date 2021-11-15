b5b0b139796cdea5595bc8d4866a9d525e66694e
o_sam <ollie.samuel11@gmail.com>
2021-11-15 12:18:07 -0500

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Main from './Main';
+import "./App.css"
+
+function App() {
+  return (
+    <div>
+      <div className="App-header">
+        <h1>Product Manager</h1>
+      </div>
+    <div className="App">
+      <Main />
+    </div>
+    </div>
+  );
+}
+export default App;
